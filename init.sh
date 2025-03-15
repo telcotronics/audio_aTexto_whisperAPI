1bd78d13ce1927467b70386b001a4c8ac68c8861
pablinux <pablinux@hotmail.es>
2025-03-15 03:30:04 -0500

    Mejorando el shell V2 de ejecucion

--- init.sh
+++ init.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 session="servidor-API"
 VENV_DIR="venv"
-EXEC_CMD="fastapi run api.py "
-cmd0="python3 -m venv api_env"
-cmd1="source api_env/bin/activate"
-cmd2="pip install whisper"
-cmd3='pip install "fastapi[standard]"'
+#EXEC_CMD="fastapi run api.py "
+
 
 echo "EJECUCIÓN INICIAL 'SI=S, NO=n'"
 read SiNo
@@ -31,16 +28,16 @@ else
 
     tmux new-session -d -s $session
     tmux new-window -t $session:$term0 -n 'API-WHISPER'
-    tmux send-keys -t $session:$term0 'sh app/'$sh1
+
     tmux send-keys -t $session:$term0 Enter
-    $EXEC_CMD
+    tmux send-keys -t $session:$term0 'npython3 -m venv api_env'
     tmux send-keys -t $session:$term0 Enter
-    $cmd0
+    tmux send-keys -t $session:$term0 'source api_env/bin/activate'
     tmux send-keys -t $session:$term0 Enter
-    $cmd1
+    tmux send-keys -t $session:$term0 'pip install whisper'
     tmux send-keys -t $session:$term0 Enter
-    $cmd2
+    tmux send-keys -t $session:$term0 'pip install "fastapi[standard]"'
     tmux send-keys -t $session:$term0 Enter
-    $cmd3
+    tmux send-keys -t $session:$term0 'fastapi run api.py'
     tmux send-keys -t $session:$term0 Enter
 fi
